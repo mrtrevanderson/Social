5a5bb159c2df5327e2f1d975e2b736577184f0f1
Trevor Anderson <trevoranderson1@hotmail.com>
2018-09-13 12:57:36 -0700

    Initial Commit

---
+++ Social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social
+//
+//  Created by Trevor Anderson on 9/13/18.
+//  Copyright © 2018 Trevor Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
