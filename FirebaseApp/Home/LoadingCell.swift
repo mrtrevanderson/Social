d1c928d1237a2aaae190fb02d119136930e0b272
Trevor Anderson <trevoranderson1@hotmail.com>
2018-12-05 19:21:08 -0800

    Updated Project Files

---
+++ FirebaseApp/Home/LoadingCell.swift
@@ -0,0 +1,31 @@
+//
+//  LoadingCell.swift
+//  FirebaseApp
+//
+//  Created by Trevor Anderson on 9/13/18.
+//  Copyright © 2018 Trevor Anderson. All rights reserved.
+//
+
+import UIKit
+
+class LoadingCell: UITableViewCell {
+
+    
+    var spinner:UIActivityIndicatorView!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        spinner = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        addSubview(spinner)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        spinner.topAnchor.constraint(equalTo: topAnchor, constant: 24.0).isActive = true
+        spinner.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -24.0).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
